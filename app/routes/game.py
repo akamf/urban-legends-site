8f83ca67443f0b01015a2356c83d4d269e094860
Andreas Eliasson <andreaseliasson91@gmail.com>
2023-12-07 16:19:15 +0100

    added game and Go-API connection

---
+++ app/routes/game.py
@@ -0,0 +1,84 @@
+import json
+import requests
+
+from flask import render_template, redirect, url_for, request
+
+from blueprints import bp_game
+from routes.utils import get_game_assets
+
+score: int = 0
+
+@bp_game.get('/rules')
+def rules_get():
+    global score
+    score = 0
+
+    return render_template('rules.html')
+
+@bp_game.get('/play')
+def the_game_get():
+    global score
+    return render_template(
+        'game.html', 
+        legends=get_game_assets('http://localhost:8000/legends/'), 
+        score=score
+    )
+
+@bp_game.post('/play')
+def the_game_post():
+    global score
+    
+    answer = request.form.get('legend')
+    if answer == 'true':
+        score += 1
+        return redirect(url_for(
+            'bp_game.the_game_get', 
+            legends=get_game_assets('http://localhost:8000/legends/'), 
+            score=score
+        ))
+    else:
+        return redirect(url_for(
+            'bp_game.final_score_get',
+            score=score
+        ))
+
+@bp_game.get('/final-score')
+def final_score_get():
+    return render_template('final-score.html', score=score)
+
+@bp_game.post('/final-score')
+def final_score_post():
+    global score
+
+    username = request.form.get('username')
+
+    response = requests.post(
+        'http://localhost:8080/score/', 
+        data=json.dumps(
+            {'Username': username, 'Score': score}
+        )
+    )
+
+    if response.status_code == 200:
+        print("POST request was successful!")
+        print("Response:", response.text)
+    else:
+        print(f"POST request failed with status code {response.status_code}")
+        print("Response:", response.text)
+
+    return redirect(url_for('bp_game.rules_get'))
+
+
+@bp_game.get('/leaderboard')
+def leaderboard():
+    response = requests.get('http://localhost:8080/scores/')
+    if response.status_code == 200:
+        data = response.json()
+        scores = sorted(data, key=lambda x: x['score'], reverse=True)
+    else:
+        print(f"Request failed with status code {response.status_code}")
+    return render_template(
+        'leaderboard.html',
+        scores=scores,
+        num_elements=len(scores)
+    )
--- app/routes/utils.py
+++ app/routes/utils.py
@@ -7,3 +7,12 @@ def get_random_legend(url: str) -> Any:
     response = requests.get(url)
     data = response.json()['legends']
     return random.choice(data)
+
+def get_game_assets(url) -> list:
+    response = requests.get(url)
+    data = response.json()['legends']
+    true = random.choice([el for el in data if el['rating'] == 'true'])
+    assets = [random.choice([el for el in data if el['rating'] != 'true']), random.choice([el for el in data if el['rating'] != 'true'])]
+    assets.append(true)
+    random.shuffle(assets)
+    return assets
